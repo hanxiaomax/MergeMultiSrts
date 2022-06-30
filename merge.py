c11c270b72c4fb0740a96ec4499962e229992484
hanxiaomax@qq.com <hanxiaomax@qq.com>
2022-06-30 22:03:37 +0800

    v0.1

---
+++ merge.py
@@ -0,0 +1,50 @@
+import os
+import re
+from datetime import datetime,timedelta
+PATTERN_INDEX = re.compile(r"^\d{1,3}$" )
+PATTERN_TIMESTAMP = re.compile(r"(?P<start>\d{2}:\d{2}:\d{2},\d{3})\s*-->\s*(?P<end>\d{2}:\d{2}:\d{2},\d{3})")
+PATTERN_EMPTYLINE = re.compile(r"\s*\n")
+srts = [file for file in os.listdir() if file.endswith(".srt") and file!="output.srt" ]
+srts = sorted(srts)
+
+offset_index = 0
+offset_timestamp = timedelta()
+output = []
+
+flag = True # new group start
+
+for srt in srts:
+    with open(srt,"r") as f:
+        print("[Parsing..]",srt)
+        for line in f.readlines():
+            index = re.match(PATTERN_INDEX,line)
+            timestamp = re.match(PATTERN_TIMESTAMP,line)
+            if index:
+                if not flag:
+                    print("+++++ERROR+++++")
+                    print("Gobal index: {} \nLocal index: {}".format(i,index.group()))
+                    print(output[-3:])
+                    print("+++++ERROR+++++")
+                i = int(index.group()) + offset_index
+                output.append(str(i)+"\n")
+                flag = False
+            elif timestamp:
+                start = datetime.strptime(timestamp.group("start"),"%H:%M:%S,%f")
+                end = datetime.strptime(timestamp.group("end"),"%H:%M:%S,%f")
+                start += offset_timestamp
+                end += offset_timestamp
+                #print(timedelta(seconds=end.second))
+                #print("{} {}".format(start,end))
+                output.append("{} ---> {}\n".format(start.strftime("%H:%M:%S,%f"),end.strftime("%H:%M:%S,%f")))
+            elif re.match(PATTERN_EMPTYLINE,line):
+                output.append(line)
+                flag = True 
+            else:
+                output.append(line)
+                
+        offset_index = i # update the index offset for next file
+        # update the timestamp offset for next file
+        offset_timestamp = timedelta(hours=end.hour,minutes=end.minute,seconds=end.second)
+
+with open("output.srt","w") as f:
+    f.writelines(output)
